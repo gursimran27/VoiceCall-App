57e083cc6b3ed63cc12f4b3491107bab535c2e59
Gursimran27 <singhgursimran165@gmail.com>
2024-08-02 12:38:15 +0530

    1

---
+++ hooks/usePlayer.js
@@ -0,0 +1,39 @@
+import {useState} from 'react'
+import { cloneDeep } from 'lodash'
+import { useSocket } from '@/context/socket'
+import { useRouter } from 'next/router'
+
+const usePlayer = (myId, roomId, peer) => {
+    const socket = useSocket()
+    const [players, setPlayers] = useState({})
+    const router = useRouter()
+    const playersCopy = cloneDeep(players)
+
+    const playerHighlighted = playersCopy[myId]
+    delete playersCopy[myId]
+
+    const nonHighlightedPlayers = playersCopy
+    console.log(nonHighlightedPlayers)
+
+    const leaveRoom = () => {
+        socket.emit('user-leave', myId, roomId)
+        console.log("leaving room", roomId)
+        peer?.disconnect();
+        router.push('/')
+    }
+
+    const toggleAudio = () => {
+        console.log("I toggled my audio")
+        setPlayers((prev) => {
+            const copy = cloneDeep(prev)
+            copy[myId].muted = !copy[myId].muted
+            return {...copy}
+        })
+        socket.emit('user-toggle-audio', myId, roomId)
+    }
+
+
+    return {players, setPlayers, playerHighlighted, nonHighlightedPlayers, toggleAudio, leaveRoom}
+}
+
+export default usePlayer;
